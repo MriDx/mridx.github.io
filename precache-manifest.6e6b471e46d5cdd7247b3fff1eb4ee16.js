767d3450b0cca48812cc5500be60ca84757463ec
mridx <mridulbaishya28@gmail.com>
2020-06-27 23:53:42 +0530

    first build

---
+++ precache-manifest.6e6b471e46d5cdd7247b3fff1eb4ee16.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b33015888d9ce416b6a80860fb7483d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e8ac34dc11be7146a69",
+    "url": "/static/css/main.099a6a36.chunk.css"
+  },
+  {
+    "revision": "eecbaee2536dba02e04d",
+    "url": "/static/js/2.14bf3ab5.chunk.js"
+  },
+  {
+    "revision": "c2496f987363ee1a26277a53c25e3675",
+    "url": "/static/js/2.14bf3ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8ac34dc11be7146a69",
+    "url": "/static/js/main.b0af1c33.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7940e6ce3d1c814582d82554510dff6b",
+    "url": "/static/media/logo.7940e6ce.png"
+  },
+  {
+    "revision": "4e96669e361c36dc71a11735b7638cec",
+    "url": "/static/media/mridx.4e96669e.jpg"
+  }
+]);
